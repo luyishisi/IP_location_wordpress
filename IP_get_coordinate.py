50c8693c1d9e10039f6134ac8b8d6890526782b0
luyishisi <543429245@qq.com>
2016-06-26 20:47:39 +0800

    项目功能基本实现,有待后期自动话加强

--- IP_get_coordinate.py
+++ IP_get_coordinate.py
@@ -4,7 +4,7 @@ IpList = open("ip_add_all.json","r").read()
 Ip = eval(IpList)
 flag = 0
 file = open('point.json','w')
-for i in range(90000):
+for i in range(2):
     ip = Ip[i]['ip']
     res = urllib2.urlopen("http://api.map.baidu.com/location/ip?ak=KrmZxHHwvLnl4Xfyt0FMMVzgGLaaxU2j&ip="+ip+"&coor=bd09ll")
     a = res.read()
@@ -12,7 +12,9 @@ for i in range(90000):
     flag += 1
     if(zidian['status'] == 0):
         print flag,ip
-        #print zidian['address']
-        print zidian['content']['point']
-        file.write(str(zidian['content']['point'])+'\n')    
+        lng =  zidian['content']['point']['x']
+        lat =  zidian['content']['point']['y']
+        #print lat,lng
+        str_temp = '{"lat": '+lat+', "lng":'+lng+'},\n'
+        file.write(str_temp)    
 file.close()
